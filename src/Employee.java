4773dac337abaabac6e15d4d4a8b0e4dbc5bc440
Chanawat Lohachala <chanawat.l@kbtg.tech>
2021-06-17 11:36:56 +0700

    Playing with constructor

--- src/Employee.java
+++ src/Employee.java
@@ -2,6 +2,15 @@ public class Employee {
     private int id;
     private String fname;
     private String lnanme;
+    private String pos;
+    //Constructor
+    public Employee(){
+        this("?","?");//call other constructor
+    }//default constructor
+    public Employee(String fname,String lnanme){
+        this.fname = fname;
+        this.lnanme = lnanme;
+    }
     public String getFname(){
         return fname+" "+lnanme;
     }
