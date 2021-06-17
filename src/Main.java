9ca4223544d57eba5a20a2988a9ee8f9522bfe16
Chanawat Lohachala <chanawat.l@kbtg.tech>
2021-06-17 11:42:42 +0700

    play with commandline

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Employee chanawat = new Employee();
+        String firstname = args[0];
+        String lastname = args[1];
+        Employee chanawat = new Employee(firstname,lastname);
         String res = chanawat.getFname();
         System.out.println(res);
     }
