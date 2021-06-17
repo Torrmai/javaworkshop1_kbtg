03b0aa7c58a88b96ec43500f2751da5baf52697b
Chanawat Lohachala <chanawat.l@kbtg.tech>
2021-06-17 15:21:58 +0700

    add MyRange

---
+++ src/MyRange.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MyRange {
+    public static void main(String[] args) {
+        int end =0 ;
+        int start = 0;
+        String[] num = args[0].split(",");
+        if('[' == (args[0].charAt(0))){
+            start += toInt(num[0]);
+        }
+        else if('(' == (args[0].charAt(0))){
+            start += toInt(num[0]) + 1;
+        }
+
+        if(']' == (args[0].charAt(args[0].length()-1))){
+            end += toInt(num[1]);
+        }
+        else if(')' == (args[0].charAt(args[0].length()-1))){
+            end += toInt(num[1]) - 1;
+        }
+        for(int i = start;i<=end;i++){
+            System.out.println(i);
+        }
+    }
+    private static int toInt(String inp){
+        String tmp = "";
+        for(int i=0;i<inp.length();i++){
+            if('[' == inp.charAt(i) | ']' == inp.charAt(i) | '(' == inp.charAt(i) | ')' == inp.charAt(i)){
+            }
+            else {tmp += inp.charAt(i);}
+        }
+        int rtn = Integer.parseInt(tmp);
+        return rtn;
+    }
+}
