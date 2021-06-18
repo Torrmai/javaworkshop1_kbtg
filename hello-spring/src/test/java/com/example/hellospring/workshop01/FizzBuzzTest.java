8dad7c095e73bed76a0e272f033b47210cefba46
Chanawat Lohachala <chanawat.l@kbtg.tech>
2021-06-18 14:50:57 +0700

    FizzBuzz problems

---
+++ hello-spring/src/test/java/com/example/hellospring/workshop01/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+package com.example.hellospring.workshop01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+    @Test
+    public void nonDivisibleBoth(){
+
+    }
+}
