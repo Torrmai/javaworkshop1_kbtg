ce109d321d9357963be869485f79072ceb40c58a
Chanawat Lohachala <chanawat.l@kbtg.tech>
2021-06-21 11:56:30 +0700

    add string handler

--- hello-res/src/main/java/com/example/hellores/employee/EmployeeController.java
+++ hello-res/src/main/java/com/example/hellores/employee/EmployeeController.java
@@ -7,7 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeController {
     @GetMapping("/employee/{id}")
-    public EmployeeResponse getEmployeeByID(@PathVariable int id){
-        return new EmployeeResponse("Chanawat","Ton",id);
+    public EmployeeResponse getEmployeeByID(@PathVariable String  id){
+        int _id=0;
+        try {
+            _id = Integer.valueOf(id);
+        }
+        catch (Exception e){
+            _id = 0;
+        }
+        return new EmployeeResponse("Chanwat","Ton",_id);
+
     }
 }
