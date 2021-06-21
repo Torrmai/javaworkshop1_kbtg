f5784ccb0805e1f655ab25a03c14b2cc8193b764
Chanawat Lohachala <chanawat.l@kbtg.tech>
2021-06-21 09:54:23 +0700

    add another spring package using gradle

---
+++ hello-res/src/main/java/com/example/hellores/HelloResApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloResApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloResApplication.class, args);
+	}
+
+}
---
+++ hello-res/src/test/java/com/example/hellores/HelloResApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloResApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
