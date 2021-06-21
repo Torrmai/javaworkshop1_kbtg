4b3fbd9c3eb48178153b6628bd6a45f5e790548a
Chanawat Lohachala <chanawat.l@kbtg.tech>
2021-06-21 14:04:16 +0700

    add spring test

--- hello-res/src/main/java/com/example/hellores/employee/EmployeeController.java
+++ hello-res/src/main/java/com/example/hellores/employee/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.hellores.employee;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,4 +19,15 @@ public class EmployeeController {
         return new EmployeeResponse("Chanwat","Ton",_id);
 
     }
+    @GetMapping("/employee")
+    public EmployeeResponse getEmployeeByID2(@RequestParam String  id) {
+        int _id = 0;
+        try {
+            _id = Integer.parseInt(id);//more thread safe
+        } catch (NumberFormatException e) {
+            _id = 0;
+        }
+        return new EmployeeResponse("Chanwat", "Ton", _id);
+    }
+
 }
