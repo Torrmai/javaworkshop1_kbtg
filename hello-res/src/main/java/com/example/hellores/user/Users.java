ac301ea787366a9947528142726d8f5ba7942715
Chanawat Lohachala <chanawat.l@kbtg.tech>
2021-06-23 14:15:00 +0700

    prepare user external api

---
+++ hello-res/src/main/java/com/example/hellores/user/Users.java
@@ -0,0 +1,15 @@
+package com.example.hellores.user;
+
+import java.util.List;
+
+public class Users {
+    private List<UserModel> users;
+
+    public List<UserModel> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<UserModel> users) {
+        this.users = users;
+    }
+}
